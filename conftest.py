bf4ae074cda1823aba8bd30f7f5864d96c7df8d5
Dmitriy Kuznetsov <dmitriy.kuznetsov1@ibm.com>
2021-02-10 21:04:03 +0200

    small fixes

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium import webdriver
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="en",
                      help="Choose website language")
-
 
 
 @pytest.fixture(scope="function")
